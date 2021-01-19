56b785452ed789f880817087faa0edadb73ac5b0
Cristóbal Montt <c.s.montt@gmail.com>
2021-01-19 21:37:08 +0000

    Update resultado_mesa.R
    updated url

--- R/resultado_mesa.R
+++ R/resultado_mesa.R
@@ -10,7 +10,7 @@
 
 resultado_mesa <- function(eleccion = "elecciones_constitucion",
                            id_mesa = 70071112){
-                  url = paste0("http://servelelecciones.cl/data/",
+                  url = paste0("http://pv.servelelecciones.cl/data/",
                         eleccion, "/computomesas/",id_mesa,".json")
                   resultado = obtener_json(url)
                   resultado
