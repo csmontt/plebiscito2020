4f9850c16ac358311602fba0c95e7e59773f7418
Cristóbal Montt <c.s.montt@gmail.com>
2021-01-19 21:35:08 +0000

    Update mesas_circ_electoral.R
    updated url

--- R/mesas_circ_electoral.R
+++ R/mesas_circ_electoral.R
@@ -8,7 +8,7 @@
 #'
 mesas_circ_electoral <- function(eleccion = "elecciones_constitucion",
                                  id_circ_elect = "7479"){
-        url = paste0("http://servelelecciones.cl/data/", eleccion,
+        url = paste0("http://pv.servelelecciones.cl/data/", eleccion,
                      "/filters/", "mesas/bycirc_electoral/",
                      id_circ_elect, ".json")
         resultado = obtener_json(url)[,1:2]
